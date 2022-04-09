b4bf50e29afdb0bc47a848aea82e4042d6da19ba
ib-bib <81387641+ib-bib@users.noreply.github.com>
2022-04-09 13:44:30 +0100

    Add files via upload

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include  # Imported the include function
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('polls', include('polls.urls')),
+    # Used the include function here to route to polls
+]
