f07cd6fe77641b7101ce92e7aea2f80cd362f38a
ib-bib <81387641+ib-bib@users.noreply.github.com>
2022-04-28 11:39:45 +0100

    Week Three Update

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include  # Imported the include function
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls', include('polls.urls')),
+    path('polls/', include('polls.urls')),
     # Used the include function here to route to polls
 ]
